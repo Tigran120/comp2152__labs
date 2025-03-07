dc75fc43dacc76a7435af89d5978f4ec442150a0
Tigran120 <Tigrankhachaturyan@georgebrown.ca>
2025-03-07 08:59:03 -0500

    Sys

--- week9/lab09/sys_operations.py
+++ week9/lab09/sys_operations.py
@@ -1,3 +1,4 @@
+import os
 import platform
 import socket
 print("Current Mchine Type")
@@ -9,7 +10,27 @@ print(platform.architecture())
 print("===========================")
 
 
-print("Current Processor Type")
+print("Set Socket TimeOut to 50 seconds")
 print(socket.setdefaulttimeout(50))
+print("Get the current Soclet Timeout")
+print(socket.getdefaulttimeout())
 print("===========================")
 
+print("Get the current Operating System Type")
+print(os.name)
+print("===========================")
+print("Get the current Operating System Name")
+print(platform.system())
+print("===========================")
+
+print("Current Process ID")
+print(os.getpid())
+print("===========================")
+
+
+file_name="fdpractise.txt"
+print(f"\n[Before  Fork] Process {os.getpid()}]")
+
+file_handle=os.open(file_name, os.O_RDWR | os.O_CREAT)
+print(f"\n[Process ID] {os.getpid()}] Opened file_handle: {file_handle}")
+print("===========================")
