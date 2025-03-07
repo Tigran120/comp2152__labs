a1d033f83ea920e0491cebb2b5ee4de1bfbb93ea
Tigran120 <Tigrankhachaturyan@georgebrown.ca>
2025-03-07 08:42:40 -0500

    Sys opeartions

---
+++ week9/lab09/sys_operations.py
@@ -0,0 +1,15 @@
+import platform
+import socket
+print("Current Mchine Type")
+print(platform.machine())
+print("===========================")
+
+print("Current Processor Type")
+print(platform.architecture())
+print("===========================")
+
+
+print("Current Processor Type")
+print(socket.setdefaulttimeout(50))
+print("===========================")
+
